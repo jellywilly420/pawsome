c0886213986d8ebe0430619adb3c2a4ca2161650
jellywilly420 <65194730+jellywilly420@users.noreply.github.com>
2024-04-23 01:48:17 +0200

    Add API for cat pics Add 50:50 chance for pet to be either dog or cat

--- scripts/api.js
+++ scripts/api.js
@@ -1,16 +1,29 @@
-const dogApi = "https://dog.ceo/api/breeds/image/random";
+const dogAPI = "https://dog.ceo/api/breeds/image/random";
+const catAPI = "https://api.thecatapi.com/v1/images/search?api_key=live_ZyVdmFuU2BzUGjqD0HqrlHRJSpmN4mSlE7Ul2xMab6u9CAJG2TTq3rqYVwl7mTYg ";
+let petAPI;
 let imageLink;
 fetchImage();
 
 
 function fetchImage () {
-    fetch(dogApi)
-.then(response => {
-    if (!response.ok) {throw new Error("not ok")};
-    return response.json();
-})
-.then(data => {
-    imageLink = data.message
+    if (Math.random() < 0.51) {
+        petAPI = dogAPI;
+    }
+    else {
+        petAPI = catAPI;
+    }
+    fetch(petAPI)
+    .then(response => {
+        if (!response.ok) {throw new Error("not ok")};
+        return response.json();
+    })
+    .then(data => {
+        if (petAPI === dogAPI) {
+            imageLink = data.message;
+        }
+        if (petAPI === catAPI) {
+            imageLink = data[0].url;
+    }
     changeImage(imageLink);
 } )
 .catch(error => {
